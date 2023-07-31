5c40f1bf839aa0e699251afa8f389c8257d3a316
sliusol <oliaslius22@gmail.com>
2023-07-31 15:01:55 +0300

    modified

---
+++ src/app/country.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { countries } from './constants/countries.data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+
+  constructor() { }
+
+  public getCountries(): Observable<string[]> {
+    return of(countries);
+  }
+}
