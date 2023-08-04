8379770c591dba9e1b576b45c98a08e2336fbcd8
sliusol <oliaslius22@gmail.com>
2023-08-04 11:21:38 +0300

    add new cva

--- src/app/shared/form-elements/select/select/select.component.ts
+++ src/app/shared/form-elements/select/select/select.component.ts
@@ -1,31 +1,59 @@
-import { Component, forwardRef } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, forwardRef, Injector, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, UntypedFormControl } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Observable } from 'rxjs';
+import { CountryService } from 'src/app/shared/services/country-service/country.service';
 
+
+
+@UntilDestroy()
 @Component({
   selector: 'app-select',
-  templateUrl: './select.component.html',
+  template: `<label class="label">{{ name }}</label>
+  <select class="select" [formControl]="control">
+    <option *ngFor="let country of countries$ | async" [value]="country">{{ country }}</option> 
+  </select>`,
   styleUrls: ['./select.component.css'],
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: forwardRef(()=> SelectComponent),
     multi:true
-  }]
+  }, [CountryService]]
 })
-export class SelectComponent implements ControlValueAccessor{
-  public value: string | undefined;
-  public changed: ((value: string) => void) | undefined;
-  public touched: (() => void) | undefined;
-
+export class SelectComponent implements ControlValueAccessor, OnInit{
+  @Input() name: string='';
   
-  constructor(){}
+  public value: string | undefined;
+  public countries$:Observable<string[]>;
+  
+  public onTouched = () => {};
+  public onChange = (value: any) => {};
+  
+  public control: UntypedFormControl;
+  
 
-  public writeValue(value: string): void {
-    this.value = value;
+  constructor(private injector: Injector, public countryService: CountryService){
+    this.control = new UntypedFormControl();
   }
+
+  public ngOnInit(): void {
+    this.countries$=this.countryService.getCountries()
+    
+    this.control.markAsTouched = function (): void {};
+
+    this.control.valueChanges.pipe(untilDestroyed(this)).subscribe((value) => {
+      this.onChange(value);  
+    });
+    }
+
+    public writeValue(obj: any): void {
+      this.control?.setValue(obj);
+      }
   public registerOnChange(fn: any): void {
-    this.changed = fn;
+    this.onChange = fn;
   }
   public registerOnTouched(fn: any): void {
-    this.touched = fn;
+    this.onTouched = fn;
   }
-}
+  }
+
