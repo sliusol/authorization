d2921ac6fc04a0921c4e8bce8ee1830fc2f4ef69
sliusol <oliaslius22@gmail.com>
2023-08-04 11:19:59 +0300

    modified

--- src/app/authcomponent/authcomponent.component.ts
+++ src/app/authcomponent/authcomponent.component.ts
@@ -1,39 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {Observable} from 'rxjs';
-import { CountryService } from '../shared/services/country-service/country.service';
-
 
 
 @Component({
   selector: 'app-authcomponent',
   templateUrl: './authcomponent.component.html',
   styleUrls: ['./authcomponent.component.css'],
-  providers: [CountryService]
 })
-export class AuthcomponentComponent implements OnInit{
-  public countries$!:Observable<string[]>;
+export class AuthcomponentComponent {
+
   public showPassword: boolean = false;
   public submittedData: any;
 
   public authForm: FormGroup;
 
-  constructor(private fb: FormBuilder, 
-    public countryService: CountryService){
+  constructor(private fb: FormBuilder){
     this.authForm = this.fb.group({
-      "firstName": ["", Validators.required],
-      "lastName": ["", Validators.required],
-      "email": ["", [Validators.required, Validators.email, Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}/)]],
-      "password": ["", [Validators.required,
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email, Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}/)]],
+      password: ["", [Validators.required,
       Validators.minLength(6),
       Validators.pattern(/^(?=.*[a-zA-Z]).+$/)]],
-      "phones": this.fb.array ([this.fb.control("", [Validators.required, Validators.pattern(/^[0-9]+$/)])]),
-      "country": ["", Validators.required],
+      phones: this.fb.array ([this.fb.control("", [Validators.required, Validators.pattern(/^[0-9]+$/)])]),
+      country: ["", Validators.required],
     });
-  }
-  
-  public ngOnInit(): void {
-    this.countries$=this.countryService.getCountries(); 
   }
 
   public  getFormsControls(): FormArray {
@@ -54,7 +45,8 @@ export class AuthcomponentComponent implements OnInit{
   public submit():void {
     if (this.authForm.valid) {
       this.submittedData = this.authForm.value;
+      
     }
-    
+    console.log(this.authForm)
   }
 }
