e1884512aa5c5543ecf0d20a198eff89f74eb1a4
sliusol <oliaslius22@gmail.com>
2023-08-04 11:22:28 +0300

    modified'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthcomponentComponent } from './authcomponent/authcomponent.component';
+import { InputComponent } from './shared/form-elements/input/input.component';
+import { SelectComponent } from './shared/form-elements/select/select/select.component';
+
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthcomponentComponent,
+    SelectComponent,
+    InputComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
