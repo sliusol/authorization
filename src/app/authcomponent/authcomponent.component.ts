0fb8bf0cf357b7e364dde716baef138fee963fc2
sliusol <oliaslius22@gmail.com>
2023-08-11 08:40:34 +0300

    validation added

--- src/app/authcomponent/authcomponent.component.ts
+++ src/app/authcomponent/authcomponent.component.ts
@@ -27,7 +27,6 @@ export class AuthcomponentComponent {
         [
           Validators.required,
           Validators.email,
-          Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}/),
         ],
       ],
       password: [
